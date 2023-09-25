4df8cc7becbb17b1a08e067a66b7a27ef081b744
chrispaladin7 <chrismartey@yahoo.com>
2023-09-25 16:28:47 -0400

    updates views.py to retrieve data for Finch Table from the DB

--- main_app/models.py
+++ main_app/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Finch(models.Model):
-        species=models.CharField(max_length=50)
-        color=models.CharField(max_length=25)
-        size=models.CharField(max_length=10)
-        description=models.TextField(max_length=250)
-        weight=models.DecimalField(...,max_digits=3, decimal_places=2)
+    species=models.CharField(max_length=50)
+    color=models.CharField(max_length=25)
+    size=models.CharField(max_length=10)
+    description=models.TextField(max_length=250)
 
-        def __str__(self):
-            return  (f'({self.id}) {self.species} Color: {self.color} ')
+    def __str__(self):
+        return  (f'({self.id}) {self.species} Color: {self.color} ')
      
--- main_app/views.py
+++ main_app/views.py
@@ -1,67 +1,6 @@
 from django.shortcuts import render
+from .models import Finch
 
-finches = [
-    {
-        'species': 'House Finch',
-        'color': 'Red',
-        'size': 'Small',
-        'description': 'Common backyard bird with red plumage.'
-    },
-    {
-        'species': 'American Goldfinch',
-        'color': 'Yellow',
-        'size': 'Small',
-        'description': 'Bright yellow bird with black wings.'
-    },
-    {
-        'species': 'European Goldfinch',
-        'color': 'Red and Yellow',
-        'size': 'Small',
-        'description': 'Distinctive red and yellow plumage.'
-    },
-    {
-        'species': 'Zebra Finch',
-        'color': 'Gray and White',
-        'size': 'Small',
-        'description': 'Small bird with striped plumage.'
-    },
-    {
-        'species': 'Purple Finch',
-        'color': 'Reddish-Purple',
-        'size': 'Small',
-        'description': 'Males have striking reddish-purple plumage.'
-    },
-    {
-        'species': 'Cassins Finch',
-        'color': 'Brown and Red',
-        'size': 'Small',
-        'description': 'Small finch with distinctive head markings.'
-    },
-    {
-        'species': 'Pine Siskin',
-        'color': 'Streaked Brown',
-        'size': 'Small',
-        'description': 'Small finch with streaked brown plumage.'
-    },
-    {
-        'species': 'Lesser Goldfinch',
-        'color': 'Black and Yellow',
-        'size': 'Small',
-        'description': 'Small finch with black and yellow plumage.'
-    },
-    {
-        'species': 'Yellow-crowned Bishop',
-        'color': 'Yellow and Black',
-        'size': 'Small',
-        'description': 'African finch with distinctive plumage.'
-    },
-    {
-        'species': 'Red-headed Finch',
-        'color': 'Red and Brown',
-        'size': 'Small',
-        'description': 'African finch with red head and brown body.'
-    },
-]
 
 # Create your views here.
 def home(request):
@@ -71,6 +10,7 @@ def about(request):
     return render(request,'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request,'finches/index.html',{
         'finches': finches
     })
