d0b0f160c6c86e120454d361aa1d08b18829932e
chrispaladin7 <chrismartey@yahoo.com>
2023-09-26 13:57:53 -0400

    adds one to many relationship between Finch and Distribution models

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+LOCATIONS=(
+    {'N','North'},
+    {'S','South'},
+    {'E','East'},
+    {'W','West'},
+)
+
 # Create your models here.
 class Finch(models.Model):
     species=models.CharField(max_length=50)
@@ -13,4 +20,24 @@ class Finch(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id':self.id})
+    
+
+class Distribution(models.Model):
+    region = models.CharField(max_length=100)
+    habitat = models.TextField()
+    migration = models.TextField(blank=True, null=True)
+    date = models.DateField()
+    location = models.CharField(
+        max_length=1,
+        choices=LOCATIONS,
+        default=LOCATIONS[0][0]
+    ) 
+    # Create a finch_id FK
+    Finch = models.ForeignKey(
+        Finch,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'Distribution of {self.get_distribution_display()} in {self.date}'
      
